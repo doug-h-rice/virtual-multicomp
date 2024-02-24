e6793cd7374f363a3582bd05d1cf33bc29d49288
doug-h-rice <doug.h.rice@btinternet.com>
2024-02-24 10:03:34 +0000

    Add files via upload

---
+++ virtual6809/doVirtual.bat
@@ -0,0 +1,12 @@
+rem
+rem doVirtual.bat builds Virtual
+rem
+rem C:\Users\Redtop\Desktop\tcc-0.9.26-win64-bin\tcc\virtual-multicomp-master\virtual6809
+rem
+rem 
+
+..\..\tcc virtual.c 6809v.c
+pause
+
+virtual doug.s19
+pause
